a3f8b06422c7549a9d245207d756455441f06493
keakon <keakon@gmail.com>
2025-04-14 14:37:13 +0800

    采用通用方案来实现 升级到 manifest V3

--- background.js
+++ background.js
@@ -1,119 +1,75 @@
-var domain_pattern = /^https?:\/\/([^\/]+)/;
-var domains_map = {};
-var script_src = chrome.runtime.getURL('enable.js');
-var inject_script =
-	"var script = document.createElement('script');\
-	script.src = '" + script_src + "';\
-	document.body.appendChild(script);"
-var script = {
-	code: inject_script,
-	allFrames: true
-};
+const STORAGE_KEY = "enabled_domains";
+const DOMAIN_PATTERN = /^https?:\/\/([^\/]+)/;
 
-function load() {
-	var domains = localStorage.domains;
-	if (domains) {
-		try {
-			domains_map = JSON.parse(statusString);
-		} catch (e) {
-			domains_map = {};
-		}
-	}
-}
-load();
-
-function addDomain(domain) {
-	if (!(domain in domains_map)) {
-		domains_map[domain] = 1;
-		localStorage.domains = JSON.stringify(domains_map);
-	}
+function getDomainFromUrl(url) {
+  if (url && url.startsWith("http")) {
+    const result = DOMAIN_PATTERN.exec(url);
+    return result ? result[1] : null;
+  }
+  return null;
 }
 
-function removeDomain(domain) {
-	if (domain in domains_map) {
-		delete domains_map[domain];
-		localStorage.domains = JSON.stringify(domains_map);
-	}
+async function needEnableCopy(url) {
+  const domain = getDomainFromUrl(url);
+  if (!domain) {
+    return false;
+  }
+  const data = await chrome.storage.local.get([STORAGE_KEY]);
+  const enabledDomains = data[STORAGE_KEY] || {};
+  return domain in enabledDomains;
 }
 
-function needEnableCopy(url) {
-	if (url && url.substr(0, 4) == 'http') {
-		var result = domain_pattern.exec(url);
-		if (result && result[1] in domains_map) {
-			return true;
-		}
-	}
-	return false;
-}
-
-chrome.tabs.onSelectionChanged.addListener(function(tabId, selectInfo) {
-	chrome.tabs.get(tabId, function(tab) {
-		chrome.browserAction.setIcon({
-			path: needEnableCopy(tab.url) ? 'icon19.png' : 'icon19-disable.png',
-			tabId: tabId
-		});
-	});
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (
+    (changeInfo.status === "loading" || changeInfo.url) &&
+    tab.url &&
+    tab.url.startsWith("http")
+  ) {
+    const currentUrl = changeInfo.url || tab.url;
+    const enabled = await needEnableCopy(currentUrl);
+    chrome.action.setIcon({
+      path: enabled ? "icon19.png" : "icon19-disable.png",
+      tabId: tabId,
+    });
+  }
 });
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	if (changeInfo.status == 'complete') {
-		if (needEnableCopy(changeInfo.url || tab.url)) {
-			chrome.tabs.executeScript(null, script);
-			chrome.browserAction.setIcon({
-				path: 'icon19.png',
-				tabId: tab.id
-			});
-		} else {
-			chrome.browserAction.setIcon({
-				path: 'icon19-disable.png',
-				tabId: tab.id
-			});
-		}
-	}
+chrome.action.onClicked.addListener(async (tab) => {
+  const domain = getDomainFromUrl(tab.url);
+  if (domain && tab.id) {
+    let enabled;
+    const data = await chrome.storage.local.get([STORAGE_KEY]);
+    const enabledDomains = data[STORAGE_KEY] || {};
+
+    if (enabledDomains[domain]) {
+      enabled = false;
+      delete enabledDomains[domain];
+    } else {
+      enabled = true;
+      enabledDomains[domain] = true;
+    }
+    await chrome.storage.local.set({ [STORAGE_KEY]: enabledDomains });
+    await chrome.action.setIcon({
+      path: enabled ? "icon19.png" : "icon19-disable.png",
+      tabId: tab.id,
+    });
+
+    await chrome.tabs.reload(tab.id);
+  }
 });
 
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-	var url = tab.url;
-	if (url && url.substr(0, 4) == 'http') {
-		var result = domain_pattern.exec(url);
-		if (result && result[1] in domains_map) {
-			removeDomain(result[1]);
-
-			chrome.browserAction.setIcon({
-				path: 'icon19-disable.png',
-				tabId: tab.id
-			});
-			return;
-		}
-
-		chrome.windows.getAll({populate: true}, function(windows) {
-			for (var i = 0; i < windows.length; ++i) {
-				var tabs = windows[i].tabs;
-				var length = tabs.length;
-				var domain = result[1];
-				for (j = 0; j < length; ++j) {
-					var tab = tabs[j];
-					var url = tab.url;
-					if (url && url.substr(0, 4) == 'http') {
-						var result2 = domain_pattern.exec(url);
-						console.log(result2[1]);
-						if (result2 && result2[1] == domain) {
-							console.log('match');
-							chrome.tabs.executeScript(tab.id, script);
-						}
-					}
-				}
-			}
-		});
-
-		addDomain(result[1]);
-	} else {
-		chrome.tabs.executeScript(null, script);
-	}
-
-	chrome.browserAction.setIcon({
-		path: 'icon19.png',
-		tabId: tab.id
-	});
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+  const tab = await chrome.tabs.get(activeInfo.tabId);
+  if (tab?.url && tab.url.startsWith("http")) {
+    const enabled = await needEnableCopy(tab.url);
+    await chrome.action.setIcon({
+      path: enabled ? "icon19.png" : "icon19-disable.png",
+      tabId: activeInfo.tabId,
+    });
+  } else {
+    await chrome.action.setIcon({
+      path: "icon19-disable.png",
+      tabId: activeInfo.tabId,
+    });
+  }
 });
---
+++ content_script.js
@@ -0,0 +1,58 @@
+const STORAGE_KEY = "enabled_domains";
+const DOMAIN_PATTERN = /^https?:\/\/([^\/]+)/;
+
+function getDomainFromUrl(url) {
+  if (url && url.startsWith("http")) {
+    const result = DOMAIN_PATTERN.exec(url);
+    return result ? result[1] : null;
+  }
+  return null;
+}
+
+function stopEventPropagation(event) {
+  event.stopImmediatePropagation();
+}
+
+const eventsToStop = [
+  "contextmenu",
+  "selectstart",
+  "copy",
+  "beforecopy",
+  "cut",
+  "mousedown",
+  "mouseup",
+  "dragstart",
+  "keydown",
+];
+
+function enableCopy() {
+  eventsToStop.forEach((eventName) => {
+    document.addEventListener(eventName, stopEventPropagation, true);
+  });
+
+  let styleElement = document.createElement("style");
+  styleElement.innerHTML = `*, *::before, *::after {
+  user-select: text !important; -webkit-user-select: text !important; cursor: auto !important;
+}
+body { overflow: auto !important; }`;
+  requestAnimationFrame(() => {
+    (document.head || document.documentElement).appendChild(styleElement);
+  });
+}
+
+async function checkInitialState() {
+  const currentUrl = window.location.href;
+  const currentDomain = getDomainFromUrl(currentUrl);
+
+  if (!currentDomain) {
+    return;
+  }
+
+  const data = await chrome.storage.local.get([STORAGE_KEY]);
+  const enabledDomains = data[STORAGE_KEY] || {};
+  if (currentDomain in enabledDomains) {
+    enableCopy();
+  }
+}
+
+checkInitialState();
